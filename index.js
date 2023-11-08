4de4393eeeda610680f2b31ba0d8d4a034e4f863
InganathiStemela <inganathistemela13@gmail.com>
2023-11-08 12:32:03 +0200

    new commmit

---
+++ index.js
@@ -0,0 +1,29 @@
+let language= 'JavaScript';
+
+let score =10;
+
+let length = 10;
+let width = 5
+console.log (length*width)
+
+//multiplication
+let number = 10*5
+console.log("The area of the rectangle is 50 where 50 is the area you calculated")
+
+let num1 = 10
+let num2 = 4
+
+//modulus
+let modulus = 10%4
+console.log(modulus)
+
+//division
+let division = 10/4
+console.log(division)
+
+
+
+
+
+
+
